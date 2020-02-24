4376a099cf1062f65f977ab70f017283a8e7d928
mrizkihidayat66 <mrizkihidayat66@gmail.com>
2020-02-24 16:25:41 +0700

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # batch-image-upscaler-cli
 Upscale your image resolution with this program. This program is written in Python with Waifu2x Engine from DeepAI API.
+
+# Requeriments
+- Python
+- Python Modules
+- DeepAI API, SignUp here (Free): https://deepai.org/
+
+# Setup
+1. Download 'waifu2x-cli.py'.
+2. Edit, replace api-key with your own API from DeepAI.
+
+# Usage
+> $ python3 waifu2x-cli.py path/your-batch-image/
---
+++ waifu2x-cli.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import shutil
+import requests
+import cv2
+import glob
+import json
+import wget
+
+input_dir   = sys.argv[1]
+output_dir  = "output"
+
+img_list = os.listdir(input_dir)
+img_list.sort()
+
+os.chdir(input_dir)
+if os.path.isdir(output_dir):
+    shutil.rmtree(output_dir)
+else:
+    os.mkdir(output_dir)
+
+i = 0
+while i < len(img_list):
+
+    print("\nWorking... "+img_list[i]+"\n")
+
+    r = requests.post(
+        "https://api.deepai.org/api/waifu2x",
+        files={
+            'image': open(img_list[i], 'rb'),
+        },
+        headers={'api-key': 'api-key:quickstart-QUdJIGlzIGNvbWluZy4uLi4K'} #Replace with your own DeepAI API. 
+    )
+    data        = json.loads(r.text)    
+
+    if data.get("status"):
+        print(data["status"])
+        break
+    else:
+        img_link    = data["output_url"]
+
+        wget.download(img_link, out=output_dir+"/upscaled_"+img_list[i])
+
+    i += 1
